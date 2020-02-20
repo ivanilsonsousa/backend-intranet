e676724a7fff847d80c0856fb4f2ce2062b705dc
Ivanilson Sousa <ivanilsonsousa95@gmail.com>
2020-02-20 18:46:07 -0300

    apagando pastas também do disco

--- src/config/deleteDir.js
+++ src/config/deleteDir.js
@@ -1,23 +1,10 @@
 const Document = require('../models/Document')
+const rimraf = require("rimraf")
 const path = require('path')
-const fs = require('fs')
 
 module.exports = async function deleteDir(req, res, next) {
-  // const document = req.body.parent == 'root' ? null : await Document.find({ _id: { $eq: req.body.parent } })
+  const document = await Document.findById( req.params.id )
+  const dir = path.resolve(`${__dirname}/../../uploads/${document.dir}/`)
 
-  // const dir_parent = document ? document[0].dir : null
-  // const { folder } = req.body
-
-  // const new_path = dir_parent ? `${dir_parent}/${folder}` : folder
-
-  // req.body.dir = new_path
-
-  // const dir = path.resolve(`${__dirname}/../../uploads/${new_path}/`)
-
-  // if (!fs.existsSync(dir)){
-  //   fs.mkdirSync(dir);
-  // }
-  
-  console.log("passou por aqui middleware...")
-  next()
+  rimraf(dir, () => next())
 }
