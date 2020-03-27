119d4327540bd17936aa993b3020c023bf8eb810
Ivanilson Sousa <ivanilsonsousa95@gmail.com>
2020-03-27 13:00:34 -0300

    adicionado PhoneController com rotas

---
+++ src/controllers/PhoneController.js
@@ -0,0 +1,39 @@
+const Phone = require('../models/Phone')
+
+module.exports = {
+    async index(req, res) {
+      const { query } = req.query
+      console.log(query)
+
+      const phone = await Phone.find({ $or: [
+        { title: new RegExp(query, 'i') },
+        { description: new RegExp(query, 'i') },
+      ]})
+      
+      return res.json(phone)
+    },
+    async show(req, res) {
+      const phone = await Phone.find({ active: true })
+
+      return res.json(phone)
+    },
+    async store(req, res) {
+      const phone = await Phone.create(req.body)
+
+      return res.json(phone)
+    },
+    async update(req, res) {
+      const { id } = req.params
+
+      const phone = await Phone.findByIdAndUpdate(id, req.body)
+
+      return res.json(phone)
+    },
+    async destroy(req, res) {
+      const { id } = req.params
+
+      const phone = await Phone.findByIdAndDelete(id)
+
+      return res.json(phone)
+    },
+}
